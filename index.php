cd23cd19d23fc36c9f2ed257ea3dd3ec5f0458be
kamonrachantks <114939766+kamonrachantks@users.noreply.github.com>
2024-01-10 10:01:31 +0700

    first commit

--- index.php
+++ index.php
@@ -94,14 +94,7 @@ if ($mode == "chklogin") {
 						</button>
 					</div>
 
-					<div class="text-center p-t-12">
-						<span class="txt1">
-							Forgot
-						</span>
-						<a class="txt2" href="#">
-							Username / Password?
-						</a>
-					</div>
+
 
 					<div class="text-center p-t-136">
 					</div>
